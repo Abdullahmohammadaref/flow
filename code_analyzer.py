3690d8cc7818046c4d777d31f01bfad7527b72f7
Abdullahmohammadaref <abdullahmohammadaref@gmail.com>
2026-05-02 20:56:57 +0300

    feat: Made code analyzer with ast parser - Created `visit_` methods in `code_analyzer.py` to collect imports, functions, classes, entry points, waiting states, decorators, global variables and inherited classes. - Added exception handling in `flow.py` for handling incorrect/corrupt zip folder path and files with invalid python code. - Wrote comments in `code_analyzer.py` and `flow.py` - Moved ast logic in `flow.py` into separate `analyze_project_data` function. - Added support for another argument "train"

--- code_analyzer.py
+++ code_analyzer.py
@@ -5,6 +5,120 @@ class CodeAnalyzer(ast.NodeVisitor):
         self.imports = []
         self.functions = []
         self.classes = []
-        self.decorators = set()
+
         self.is_entry_point = False
         self.has_wait_state = False
+
+        self.decorators = set()
+
+        self.inherited_classes = set()
+        self.global_variables = set()
+
+    def visit_Import(self, node):
+        """
+        Handels normal imports like "import sys" or "import sys, os"
+        """
+        for imports in node.names:
+            self.imports.append(imports.name)
+
+    def visit_ImportFrom(self, node):
+        """
+        Handle imports from a library like "from sys import argv"
+        """
+        # Safety fallback to handle relative imports like "from . import utils"
+        module_name = node.module if node.module else ""
+
+        for import_name in node.names:
+            self.imports.append(f"{module_name}.{import_name.name}")
+
+    def visit_FunctionDef(self, node):
+        """
+        Finds function names and decorators
+        """
+        self.functions.append(node.name)
+
+        # Look for decorators used on function
+        for decorator in node.decorator_list:
+            # If the decorator is a Nme decorator then add the decorator name to the decorators set.
+            if isinstance(decorator, ast.Name):
+                self.decorators.add(decorator.id)
+            # Else if the decorator is a Call and the called then we dig inside the function to get the name of the decorator.
+            elif isinstance(decorator, ast.Call) and isinstance(decorator.func, ast.Name):
+                self.decorators.add(decorator.func.id)
+
+        # Look inside node for other nested nodes
+        self.generic_visit(node)
+
+    def visit_ClassDef(self, node):
+        """
+        Finds class names and inherited classes
+        """
+        self.classes.append(node.name)
+
+        # Look for classes that this class inherits
+        for inherited_class in node.bases:
+            # If class name is inherited directly
+            if isinstance(inherited_class, ast.Name):
+                self.inherited_classes.add(inherited_class.id)
+
+            # If module name is called before inherited class name
+            elif isinstance(inherited_class, ast.Attribute):
+                self.inherited_classes.add(inherited_class.attr)
+
+            # If a function with a class is inherited
+            elif isinstance(inherited_class, ast.Call) and isinstance(inherited_class.func, ast.Name):
+                self.inherited_classes.add(inherited_class.func.id)
+
+        self.generic_visit(node)
+
+    def visit_Call(self, node):
+        """
+        Check if the name of a function matches the name of one of the waiting states functions
+        """
+        function_name = ""
+        if isinstance(node.func, ast.Name):
+            function_name = node.func.id
+        elif isinstance(node.func, ast.Attribute):
+            function_name = node.func.attr
+
+        if function_name in [
+            "listen", # for sockets
+            "mainloop", # for tkinter
+            "run", # for flask
+            "run_forever", # for asyncio
+            "serve_forever", # for http.server or socketserver libraries
+        ]:
+            self.has_wait_state = True
+
+        self.generic_visit(node)
+
+    def visit_If(self, node):
+        """
+        Looks for entry point if __name__ == "__main__" or if "__main__" == __name__
+        """
+        # Check if the "if" statement is a compare statement with two comparators
+        if isinstance(node.test, ast.Compare) and len(node.test.comparators) == 1:
+
+            # Take the left and the right side of the compare statement
+            left_side = node.test.left
+            right_side = node.test.comparators[0]
+
+            # Checks if the compared values are the same to those compared in the entry point
+            entry_point_variant_1 = (isinstance(left_side, ast.Name) and left_side.id == "__name__") and (isinstance(right_side, ast.Constant) and right_side.value == "__main__")
+            entry_point_variant_2 = (isinstance(left_side, ast.Constant) and left_side.value == "__main__") and (isinstance(right_side, ast.Name) and right_side.id == "__name__")
+
+            # If any entry point variable is true then this if statement represents an entrypoint
+            if entry_point_variant_1 or entry_point_variant_2:
+                self.is_entry_point = True
+
+        self.generic_visit(node)
+
+    def visit_Assign(self, node):
+        if node.col_offset == 0:
+            for variable in node.targets:
+                if isinstance(variable, ast.Name):
+                    self.global_variables.add(variable.id)
+        self.generic_visit(node)
+
+
+
--- flow.py
+++ flow.py
@@ -5,46 +5,112 @@ import ast
 from code_analyzer import CodeAnalyzer
 
 def encode_zip(zip_folder_path: str):
-    with open(zip_folder_path, "rb") as zip_folder_bytes:
-        return zip_folder_bytes.read()
+    """
+    Encode a zip file
+
+    :param: zip_folder_path:
+    :return: a zip folder in bytes format
+    """
+    try:
+        with open(zip_folder_path, "rb") as zip_folder_bytes:
+            return zip_folder_bytes.read()
+    except FileNotFoundError:
+        print(f"Zip folder not found at {zip_folder_path}")
+        sys.exit()
 
 def unzip_project(zip_folder_bytes: bytes):
+    """
+    - Encode zip folder bytes into a folder object
+    - Extract only relevant .py files
+    - Reads file content as string
+
+    :param: a zip folder in bytes format
+    :return: a list of .py file paths and their content as strings
+    """
     try:
         with zipfile.ZipFile(io.BytesIO(zip_folder_bytes)) as folder:
             files = {}
             for file_name in folder.namelist():
-                if file_name in ["venv", "__pycache__", "mirations"]:
+                # Remove unwanted folders that contain .py files
+                if any(i in file_name for i in ["venv", "__pycache__", "migrations"]):
                     continue
                 if file_name.endswith(".py"):
                     files[file_name] = folder.read(file_name).decode("utf-8")
             return files
-    except zipfile.BadZipfile:
-        return {}
+    except zipfile.BadZipFile:
+        print("Couldn't unzip project folder: the zip file is corrupt")
+        sys.exit()
+
+def analyze_project_data(project):
+    """
+    Analyze project files using ast library
+
+    :param: a dictionary of file paths and their content as strings
+    :return: a dictionary of file data
+    """
+    data = {}
+    for file_name, file_content in project.items():
+        try:
+            # Use ast to parse the python file code
+            file_tree = ast.parse(file_content)
+
+            code_analyzer = CodeAnalyzer()
+
+            # Visit only created "visit_nodeName" methods
+            code_analyzer.visit(file_tree)
+
+            data[file_name] = {
+                "imports": code_analyzer.imports,
+                "functions": code_analyzer.functions,
+                "classes": code_analyzer.classes,
+
+                "is_entry_point": code_analyzer.is_entry_point,
+                "has_wait_state": code_analyzer.has_wait_state,
+
+                "decorators": list(code_analyzer.decorators),
+
+                "base_classes": list(code_analyzer.inherited_classes),
+                "global_variables": list(code_analyzer.global_variables)
+            }
+        except SyntaxError:
+            # Skip files with invalid python code
+            continue
+    return data
 
 def visualize(project_zip_folder_bytes: bytes):
+    """
+    Main function for handling all visualization steps in order by calling functions for the visualization process:
+    1- Unzipping project and extracting data using ast
+    2- Build graph with networkx
+    3- Further Analyze graph
+    4- Runs the graph in our Prediction model after loading .pkl training data.
+    5- Output the final graph in json format
+
+    :param: project_zip_folder_bytes:
+    :return: a JSON project flow diagram that is ready for drawing/visualizing
+    """
+    # Step 1: Unzipping project and extracting data using ast
     project = unzip_project(project_zip_folder_bytes)
+    project_data = analyze_project_data(project)
 
-    project_data = {}
-    for file_name, file_content in project.items():
+    # Step 2: Build graph with networkx
 
-        file_tree = ast.parse(file_content)
-        code_analyzer = CodeAnalyzer()
-        code_analyzer.visit(file_tree)
+def train():
+    """
+    Trains our model with new repositories each time this function is called
+    1- Gather repositories with GitHub API and create a .csv data frame.
+    2- Training the model and save train data in .pickle file
+    """
+    pass
 
-        project_data[file_name] = {
-            "imports": code_analyzer.imports,
-            "functions": code_analyzer.functions,
-            "classes": code_analyzer.classes,
-
-            "is_entry_point": code_analyzer.is_entry_point,
-            "has_wait_state": code_analyzer.has_wait_state,
-
-            "decorators": list(code_analyzer.decorators),
-        }
-
-
-def main(project_zip_folder_path: str):
-    visualize(encode_zip(project_zip_folder_path))
+def main():
+     if len(sys.argv) > 1:
+        if sys.argv[1].lower() == "train":
+            train()
+        else:
+            visualize(encode_zip(sys.argv[1]))
+     else:
+        print("No argument provided")
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
